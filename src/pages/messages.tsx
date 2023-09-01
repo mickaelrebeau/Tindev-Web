82ed115fec13792176aa999e66e8665b1743c159
Forthtilliath <forth@live.fr>
2023-09-01 14:46:42 +0200

    💄 style: add margin under tilte

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -5,7 +5,7 @@ import { messages2 } from '@/lib/data/messages'
 export function Messages() {
   return (
     <section className="text-slate-100">
-      <SecondaryTitle>Messages</SecondaryTitle>
+      <SecondaryTitle className="mb-3">Messages</SecondaryTitle>
 
       {messages2.map((message) => (
         <Message key={message.id} {...message} id={message.id} />
