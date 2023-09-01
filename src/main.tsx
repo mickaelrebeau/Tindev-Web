c6b7e62c96b8d0189c8a2cf985c7dea69b3df310
Forthtilliath <forth.developer@gmail.com>
2023-09-01 07:06:50 +0200

    🔧 refactor: add queryClient config file

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import '@/index.css'
 import { router } from '@/router'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 10,
-    },
-  },
-})
+import { queryClient } from './lib/queryClient.config'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
