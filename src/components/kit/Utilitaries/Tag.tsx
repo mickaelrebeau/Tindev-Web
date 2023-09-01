5e4ec5cf2e44d21546a6543d2021a8c224426027
Forthtilliath <forth.developer@gmail.com>
2023-09-01 07:53:29 +0200

    🩹 refactor: change some html balises

--- src/components/kit/Utilitaries/Tag.tsx
+++ src/components/kit/Utilitaries/Tag.tsx
@@ -1,6 +1,7 @@
 export function Tag({ content }: { content: string }) {
-
-    return (
-        <span className="text-sm md:text-base text-sky-600 border border-sky-600 rounded-full px-3 py-1 dark:text-zinc-200 dark:border-zinc-200 text-center">{content}</span>
-    )
+  return (
+    <li className="text-sm md:text-base text-sky-600 border border-sky-600 rounded-full px-3 py-1 dark:text-zinc-200 dark:border-zinc-200 text-center list-none">
+      {content}
+    </li>
+  )
 }
