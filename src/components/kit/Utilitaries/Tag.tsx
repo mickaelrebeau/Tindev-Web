58e8efd0f106fd8db954b5b36cbcd353e02411af
Forthtilliath <forth@live.fr>
2023-09-02 01:56:15 +0200

    🔨 refactor: add some exemples of tag

--- src/components/kit/Utilitaries/Tag.tsx
+++ src/components/kit/Utilitaries/Tag.tsx
@@ -7,7 +7,7 @@ export function Tag({ children, className, ...props }: Props) {
   return (
     <li
       className={cn(
-        'text-sm md:text-base text-sky-600 border border-sky-600 rounded-full px-3 py-1 dark:text-zinc-200 dark:border-zinc-200 text-center list-none',
+        'text-sm md:text-base w-fit text-sky-600 border border-sky-600 rounded-full px-3 py-1 dark:text-zinc-200 dark:border-zinc-200 text-center list-none inline-block',
         className
       )}
       {...props}>
