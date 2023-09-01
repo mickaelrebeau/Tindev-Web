710bf79395091bccc66692cf8447b6c233c0252a
Forthtilliath <forth.developer@gmail.com>
2023-09-01 06:45:17 +0200

    ♻️ refactor: add default type as button for Buttons

--- src/components/kit/Button/Button.tsx
+++ src/components/kit/Button/Button.tsx
@@ -1,12 +1,20 @@
 import type { ButtonHTMLAttributes, PropsWithChildren } from 'react'
 import { cn } from '@/lib/utils'
 
-export type ButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>
+export type ButtonProps = PropsWithChildren<
+  ButtonHTMLAttributes<HTMLButtonElement>
+>
 
-export default function Button({ children, className, ...props }: ButtonProps) {
+export default function Button({
+  children,
+  className,
+  type = 'button',
+  ...props
+}: ButtonProps) {
   return (
     <button
       className={cn('text-lg w-fit px-3 py-2 m-4 rounded-md', className)}
+      type={type}
       {...props}>
       {children}
     </button>
--- src/pages/kit.tsx
+++ src/pages/kit.tsx
@@ -1,41 +1,47 @@
-import { PrimaryButton } from "@/components/kit/Button/PrimaryButton";
-import { SecondaryButton } from "@/components/kit/Button/SecondaryButton";
-import { TertiaryButton } from "@/components/kit/Button/TertiaryButton";
-import { Paragraph } from "@/components/kit/Utilitaries/Paragraph";
-import { FirstTitle } from "@/components/kit/Title/FirstTitle";
-import { SecondaryTitle } from "@/components/kit/Title/SecondarytTitle";
-import { Tag } from "@/components/kit/Utilitaries/Tag";
-import { Card } from "@/components/card/Card";
-import image from "@/assets/moi.jpg";
-
+import { PrimaryButton } from '@/components/kit/Button/PrimaryButton'
+import { SecondaryButton } from '@/components/kit/Button/SecondaryButton'
+import { TertiaryButton } from '@/components/kit/Button/TertiaryButton'
+import { Paragraph } from '@/components/kit/Utilitaries/Paragraph'
+import { FirstTitle } from '@/components/kit/Title/FirstTitle'
+import { SecondaryTitle } from '@/components/kit/Title/SecondarytTitle'
+import { Tag } from '@/components/kit/Utilitaries/Tag'
+import { Card } from '@/components/card/Card'
+import image from '@/assets/moi.jpg'
 
 export function Kit() {
-    return (
-        <div className="">
-            <FirstTitle
-                title="Titre 1"
-            />
-            <SecondaryTitle
-                title="Titre 2"
-            />
-            <PrimaryButton type="button">Button 1</PrimaryButton>
-            <SecondaryButton type="button">Button 2</SecondaryButton>
-            <TertiaryButton type="button">Button 3</TertiaryButton>
-            <Paragraph
-                content="Nostrud ea eu elit sint pariatur culpa proident consequat consectetur. Cupidatat ea deserunt ut ut esse consectetur minim sit consequat nisi. Incididunt cupidatat Lorem sunt esse aliqua qui exercitation esse ex veniam non. Excepteur eu sit reprehenderit commodo consectetur voluptate duis nulla exercitation esse."
-            />
-            <div className="">
-                <Tag
-                    content="Python"
-                />
-            </div>
-            <Card 
-                pseudo={"Raynh"} 
-                description={"Nostrud ea eu elit sint pariatur culpa proident consequat consectetur. Cupidatat ea deserunt ut ut esse consectetur minim sit consequat nisi. Incididunt cupidatat Lorem sunt esse aliqua qui exercitation esse ex veniam non. Excepteur eu sit reprehenderit commodo consectetur voluptate duis nulla exercitation esse."} 
-                avatar={image} 
-                tags={["Python","Java","C","HTML","CSS","JavaScript","ReactJS","Tailwind"]} 
-                reseaux={[{name:"github",link:"https://github.com/RaphHuynh"},{name:"instagram",link:"https://github.com/RaphHuynh"},{name:"twitch",link:"https://github.com/RaphHuynh"}]}            
-            />
-        </div>
-    )
+  return (
+    <div className="">
+      <FirstTitle title="Titre 1" />
+      <SecondaryTitle title="Titre 2" />
+      <PrimaryButton>Button 1</PrimaryButton>
+      <SecondaryButton>Button 2</SecondaryButton>
+      <TertiaryButton>Button 3</TertiaryButton>
+      <Paragraph content="Nostrud ea eu elit sint pariatur culpa proident consequat consectetur. Cupidatat ea deserunt ut ut esse consectetur minim sit consequat nisi. Incididunt cupidatat Lorem sunt esse aliqua qui exercitation esse ex veniam non. Excepteur eu sit reprehenderit commodo consectetur voluptate duis nulla exercitation esse." />
+      <div className="">
+        <Tag content="Python" />
+      </div>
+      <Card
+        pseudo={'Raynh'}
+        description={
+          'Nostrud ea eu elit sint pariatur culpa proident consequat consectetur. Cupidatat ea deserunt ut ut esse consectetur minim sit consequat nisi. Incididunt cupidatat Lorem sunt esse aliqua qui exercitation esse ex veniam non. Excepteur eu sit reprehenderit commodo consectetur voluptate duis nulla exercitation esse.'
+        }
+        avatar={image}
+        tags={[
+          'Python',
+          'Java',
+          'C',
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'ReactJS',
+          'Tailwind',
+        ]}
+        reseaux={[
+          { name: 'github', link: 'https://github.com/RaphHuynh' },
+          { name: 'instagram', link: 'https://github.com/RaphHuynh' },
+          { name: 'twitch', link: 'https://github.com/RaphHuynh' },
+        ]}
+      />
+    </div>
+  )
 }
