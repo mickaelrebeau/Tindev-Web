b7c605e67331224ceb68c6a0ed00d259ed58a994
Snox <hvangermee@gmail.com>
2023-09-01 11:43:40 +0200

    Snox (#2)
    * Add tailwind configuration
    
    * add: Tailwind config, modify: Card.jsx

--- src/components/kit/Utilitaries/Tag.tsx
+++ src/components/kit/Utilitaries/Tag.tsx
@@ -1,6 +1,8 @@
-export function Tag({ content }: { content: string }) {
+export function Tag({ content, style }: { content: string, style: any }) {
   return (
-    <li className="text-sm md:text-base text-sky-600 border border-sky-600 rounded-full px-3 py-1 dark:text-zinc-200 dark:border-zinc-200 text-center list-none">
+    <li className="text-sm md:text-base text-sky-600 border border-sky-600 rounded-full px-3 py-1 dark:text-zinc-200 dark:border-zinc-200 text-center list-none" style={{
+      ...style
+    }}>
       {content}
     </li>
   )
