a99646922e6c2f8afc815e2811641e0f57a91fef
Forthtilliath <forth@live.fr>
2023-09-02 20:41:16 +0200

    ♻️refactor: remove old data to add new

--- src/lib/data/messages.ts
+++ src/lib/data/messages.ts
@@ -1,23 +1,82 @@
-export const messages2: Message2[] = [
+import { generateSortedDates } from '../methods/date'
+
+const ids = Array.from({ length: 100 }, (_, i) => i)
+const idsIt = ids[Symbol.iterator]()
+
+const dates = generateSortedDates(20)
+const datesIt = dates[Symbol.iterator]()
+
+export const messages = [
   {
-    id: 0,
-    userId: 0,
-    date: 'Feb. 8, 2022',
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 1,
+    date: datesIt.next().value,
+    content: 'Un petit message ?',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 1,
+    date: datesIt.next().value,
     content:
-      'Very straight-to-point article. Really worth time reading. Thank you! But tools are just the instruments for the UX designers. The knowledge of the design tools are as important as the creation of the design strategy.',
-    comments: [
-      {
-        userId: 2,
-        date: 'Mar. 12, 2022',
-        content:
-          'The article covers the essentials, challenges, myths and stages the UX designer should consider while creating the design strategy.',
-      },
-      {
-        userId: 0,
-        date: 'Jun. 23, 2022',
-        content:
-          'Thanks for sharing this. I do came from the Backend development and explored some of the tools to design my Side Projects.',
-      },
-    ],
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl eu nisl. Nullam euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl eu nisl.',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 0,
+    date: datesIt.next().value,
+    content: 'Petit message mignon !',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 0,
+    date: datesIt.next().value,
+    content:
+      'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl eu nisl. Nullam euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, quis aliquam nisl nisl eu nisl.',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 0,
+    date: datesIt.next().value,
+    content: 'Tu fais quoi ?',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 1,
+    date: datesIt.next().value,
+    content: 'Je dev un snake en dodge ! et toi ?',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 0,
+    date: datesIt.next().value,
+    content: "Ouah ! ca a l'air super cool !",
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 0,
+    date: datesIt.next().value,
+    content: 'Moi je suis en train de faire un site de rencontre !',
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 1,
+    date: datesIt.next().value,
+    content: "Ah bon ? c'est quoi le nom ?",
+  },
+  {
+    channelId: 0,
+    id: idsIt.next().value,
+    userId: 0,
+    date: datesIt.next().value,
+    content: "C'est Tindev ! l'app de rencontre pour les devs !",
   },
 ]
