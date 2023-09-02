55a99bec07b735d9413bf6dec0b71f54a02919d0
Forthtilliath <forth@live.fr>
2023-09-02 16:07:31 +0200

    🔨refactor: add function to generate sorted dates

--- src/lib/methods/date.ts
+++ src/lib/methods/date.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 export function formatDate(date: Date) {
   return date.toLocaleDateString('fr-fr', {
     year: 'numeric',
@@ -6,4 +8,10 @@ export function formatDate(date: Date) {
     hour: '2-digit',
     minute: '2-digit',
   })
+}
+
+export function generateSortedDates(nb: number) {
+  return Array.from({ length: nb })
+    .map(() => faker.date.past())
+    .sort((a, b) => a.getTime() - b.getTime())
 }
