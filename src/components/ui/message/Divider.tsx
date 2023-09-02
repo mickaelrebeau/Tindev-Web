1ee6f7f7df7ceb976885ca553b45fe6f5cad4389
Forthtilliath <forth@live.fr>
2023-09-02 20:38:52 +0200

    ♻️refactor: add DividerAuto to format text from date

--- src/components/ui/message/Divider.tsx
+++ src/components/ui/message/Divider.tsx
@@ -1,13 +1,44 @@
-export const Divider = ({ date = "Aujourd'hui" }) => {
+import {
+  formatDate,
+  getDayOfWeek,
+  isSameDay,
+  isYesterday,
+} from '@/lib/methods/date'
+
+type DividerProps = {
+  text?: string
+}
+
+export const Divider = ({ text = '' }: DividerProps) => {
   return (
     <>
       <div className={'flex items-center px-12'}>
         <div className={'w-full h-px bg-slate-800 '} />
 
-        <p className={'text-slate-300 text-sm mt-1 mx-6 min-w-fit'}>{date}</p>
+        <p className={'text-slate-300 text-sm mt-1 mx-6 min-w-fit'}>{text}</p>
 
         <div className={'w-full h-px bg-slate-800'} />
       </div>
     </>
   )
+}
+
+type DividerAutoProps = {
+  date: Date
+}
+export function DividerAuto({ date }: DividerAutoProps) {
+  if (isSameDay(date, new Date())) {
+    return <Divider text="Aujourd'hui 🌞" />
+  }
+
+  if (isYesterday(date)) {
+    return <Divider text="Hier" />
+  }
+
+  const dayName = getDayOfWeek(date)
+  if (dayName) {
+    return <Divider text={dayName} />
+  }
+
+  return <Divider text={formatDate(date)} />
 }
