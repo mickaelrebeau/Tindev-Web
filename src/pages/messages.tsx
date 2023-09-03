9d8dd926fbdac4aaa024ee4f2f4914758ef38dde
Forthtilliath <forth@live.fr>
2023-09-03 18:56:52 +0200

    💄style: add scrollbar on hover & change style

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -21,8 +21,6 @@ export function Messages() {
     <>
       <div className="min-h-[calc(100vh-50vh)] min-w-[calc(100vw-50vw)] max-w-[780px] max-h-[780px] w-full h-full md:min-h-[780px] md:min-w-[780px] rounded-full absolute top-1/3 -z-1 gradient-1 blur-[150px] -translate-y-1/2 -translate-x-1/2 left-2/3" />
       <div className="w-full h-full max-w-[394px] max-h-[560px] gradient-2 absolute top-1/2 -translate-x-1/2 left-1/3 opacity-100 blur-[150px] translate-y-1/2 lg:translate-y-1/3 -z-1" />
-      {/* TODO: Remove `pb-20` */}
-      {/* <div className="grid grid-cols-messages grid-rows-messages divide-x-2 h-[calc(100vh-6rem)] overflow-hidden"> */}
       <div className="grid grid-cols-messages grid-rows-messages h-[inherit]">
         <div className="row-start-1 row-end-2 col-start-1 col-end-2">
           Profile
@@ -33,9 +31,8 @@ export function Messages() {
         <div className="row-start-3 row-end-4 col-start-1 col-end-2">
           Messages
         </div>
-        <div className="row-start-1 row-end-4 col-start-2 col-end-3 border-l-2 border-r-2 overflow-auto">
-          {/* <div className="w-full h-full overflow-auto px-2 flex flex-col gap-4"> */}
-          <div className="w-full px-2 flex flex-col gap-4">
+        <div className="row-start-1 row-end-4 col-start-2 col-end-3 border-l-2 border-r-2 h-[inherit]">
+          <div className="w-full px-2 flex flex-col gap-4 h-[inherit] scrollbar-stable overflow-y-hidden round hover:overflow-y-auto scrollbar-w-3 scrollbar-rounded-track scrollbar-rounded-thumb scrollbar-shadow-track scrollbar-shadow-thumb">
             {messages.map((message) => {
               const isSameDateThanPrevious = lastDate.current
                 ? isSameDay(lastDate.current, message.date)
