12eb8a6c2b62f8671233fb8613078dcecaa9ad53
Forthtilliath <forth@live.fr>
2023-09-03 16:32:30 +0200

    💄style: add logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,10 @@
-import { DiCodeigniter } from 'react-icons/di'
+import logo from '@/assets/logo.png'
 import { NavItem } from '../ui/NavItem'
 
 export function NavBar() {
   return (
     <div className="bg-gray-900 text-white py-3 px-4 flex items-center gap-4 h-15">
-      <div className="">
-        <DiCodeigniter />
-        {/* <img src="logo.png" alt="Logo" className="w-8 h-8" /> */}
-      </div>
+      <img src={logo} alt="Logo" className="h-10 -mt-2" />
 
       <nav className="md:flex md:gap-6">
         <NavItem to="/">Accueil</NavItem>
