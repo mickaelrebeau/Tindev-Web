6ad934c42afcdc60124b65aca1b3b3f8b59f1ca9
Forthtilliath <forth@live.fr>
2023-09-04 15:42:29 +0200

    ♻️refactor: add classname property

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,14 @@
-export function Footer() {
+import { cn } from '@/lib/utils'
+
+type Props = {
+  className?: string
+}
+
+export function Footer({ className }: Props) {
   return (
     <>
-      <footer className="bg-gray-900 text-white py-4 px-4 h-15">
+      <footer
+        className={cn('bg-gray-900 text-white py-4 px-4 h-15', className)}>
         <p>Projet de Mike, Nora et Rayn</p>
       </footer>
     </>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,18 @@
+import { cn } from '@/lib/utils'
 import logo from '@/assets/logo.png'
 import { NavItem } from '../ui/NavItem'
 
-export function NavBar() {
+type Props = {
+  className?: string
+}
+
+export function NavBar({ className }: Props) {
   return (
-    <div className="bg-gray-900 text-white py-3 px-4 flex items-center gap-4 h-15">
+    <div
+      className={cn(
+        'bg-gray-900 text-white py-3 px-4 flex items-center gap-4 h-15',
+        className
+      )}>
       <img src={logo} alt="Logo" className="h-10 -mt-2" />
 
       <nav className="md:flex md:gap-6">
