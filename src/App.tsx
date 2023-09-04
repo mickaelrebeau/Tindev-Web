8329eade65f468f58da7db24519c44c8ca08f95b
Forthtilliath <forth@live.fr>
2023-09-04 15:41:58 +0200

    💄style: add flex class until remove height

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,21 @@ export function App() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <NavBar />
+      <NavBar className="flex-initial" />
       {/* TODO: Mettre ca directement sur section */}
       <div id="background"></div>
       <section
-        className={cn('text-slate-100 h-[calc(100vh_-_60px_-_60px)]', {
-          'p-12 w-9/12 max-w-screen-xl m-auto overflow-auto':
-            pathname !== '/messages',
-        })}>
+        className={cn(
+          'text-slate-100 flex-auto h-[calc(100vh_-_60px_-_60px)]',
+          {
+            // className={cn('text-slate-100 flex-auto', {
+            'p-12 w-9/12 max-w-screen-xl m-auto overflow-auto':
+              pathname !== '/messages',
+          }
+        )}>
         <Outlet />
       </section>
-      <Footer />
+      <Footer className="flex-initial" />
     </div>
   )
 }
