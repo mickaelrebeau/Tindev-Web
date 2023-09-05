82e4cf304a73e8dfb0570659c8d33bf0a04d3e98
Forthtilliath <forth@live.fr>
2023-09-05 12:33:42 +0200

    🔨refactor: add LinearGradient & variants components

---
+++ src/components/ui/background/LinearGradient.tsx
@@ -0,0 +1,42 @@
+import { cn } from '@/lib/utils'
+
+//? Version avec string, sans controle de la classe from-color et to-color
+
+type Props = {
+  from: `from-${string}`
+  to: `to-${string}`
+  className?: string
+}
+
+export function LinearGradient({ from, to, className }: Props) {
+  return (
+    <div
+      className={cn(
+        'absolute -z-1 h-full w-full bg-gradient-to-b blur-[150px]',
+        from,
+        to,
+        className
+      )}
+    />
+  )
+}
+
+export function LinearGradientBlueToPink() {
+  return (
+    <LinearGradient
+      from="from-[#00c2ff]"
+      to="to-[#ff29c3]"
+      className="left-2/3 top-1/3 max-h-[780px] min-h-[calc(100vh-50vh)] min-w-[calc(100vw-50vw)] max-w-[780px] -translate-x-1/2 -translate-y-1/2 rounded-full  blur-[150px] md:min-h-[780px] md:min-w-[780px]"
+    />
+  )
+}
+
+export function LinearGradientBlueToBlue() {
+  return (
+    <LinearGradient
+      from="from-[#184bff]"
+      to="to-[#174aff]"
+      className="left-1/3 top-1/2 max-h-[560px] max-w-[394px] -translate-x-1/2 translate-y-1/2 opacity-100 blur-[150px] lg:translate-y-1/3"
+    />
+  )
+}
--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,3 +1,7 @@
+import {
+  LinearGradientBlueToBlue,
+  LinearGradientBlueToPink,
+} from '@/components/ui/background/LinearGradient'
 import { Chat } from '@/components/ui/message/Chat'
 import { MatchProfile } from '@/components/ui/message/MatchProfile'
 
@@ -12,22 +16,22 @@ import { MatchProfile } from '@/components/ui/message/MatchProfile'
 export function Messages() {
   return (
     <>
-      <div className="min-h-[calc(100vh-50vh)] min-w-[calc(100vw-50vw)] max-w-[780px] max-h-[780px] w-full h-full md:min-h-[780px] md:min-w-[780px] rounded-full absolute top-1/3 -z-1 gradient-1 blur-[150px] -translate-y-1/2 -translate-x-1/2 left-2/3" />
-      <div className="w-full h-full max-w-[394px] max-h-[560px] gradient-2 absolute top-1/2 -translate-x-1/2 left-1/3 opacity-100 blur-[150px] translate-y-1/2 lg:translate-y-1/3 -z-1" />
-      <div className="grid grid-cols-messages grid-rows-messages h-[inherit]">
-        <div className="row-start-1 row-end-2 col-start-1 col-end-2">
+      <LinearGradientBlueToPink />
+      <LinearGradientBlueToBlue />
+      <div className="grid h-[inherit] grid-cols-messages grid-rows-messages">
+        <div className="col-start-1 col-end-2 row-start-1 row-end-2">
           Profile
         </div>
-        <div className="row-start-2 row-end-3 col-start-1 col-end-2">
+        <div className="col-start-1 col-end-2 row-start-2 row-end-3">
           Discover
         </div>
-        <div className="row-start-3 row-end-4 col-start-1 col-end-2">
+        <div className="col-start-1 col-end-2 row-start-3 row-end-4">
           Messages
         </div>
-        <div className="row-start-1 row-end-4 col-start-2 col-end-3 border-l-2 border-r-2 h-[inherit]">
+        <div className="col-start-2 col-end-3 row-start-1 row-end-4 h-[inherit] border-l-2 border-r-2">
           <Chat />
         </div>
-        <div className="row-start-1 row-end-4 col-start-3 col-end-4">
+        <div className="col-start-3 col-end-4 row-start-1 row-end-4">
           <MatchProfile />
         </div>
       </div>
