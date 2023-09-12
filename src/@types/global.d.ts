d6ea94dcafaf4da3c9b3d1b7c78e63650c5a3962
Forthtilliath <forth@live.fr>
2023-09-12 21:51:09 +0200

    🛠️ feat(utils): add utility type

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,6 +3,10 @@ import { dict_network } from '@/lib/data/network'
 declare global {
   type NetworkName = (typeof dict_network)[number]['name']
   type Network = { name: NetworkName; link: string }
+
+  type PropsWithClassname<T = unknown> = T & {
+    className?: string
+  }
 }
 
 export {}
