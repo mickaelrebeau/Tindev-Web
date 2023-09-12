c71f35b306bd08f3befb7ddabe82b87cbe664852
Forthtilliath <forth@live.fr>
2023-09-12 22:57:37 +0200

    🔀 fix(query): correct query name

--- src/lib/queries/messages.ts
+++ src/lib/queries/messages.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import * as Messages from '@/lib/api/messages'
 
 export function useChannelsSummary(userId: User['id']) {
-  return useQuery<ChannelSummary[], Error>(['user', userId], () =>
+  return useQuery<ChannelSummary[], Error>(['channels-summary', userId], () =>
     Messages.getChannelsSummary(userId)
   )
 }
